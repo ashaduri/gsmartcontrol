0cf584877219c40510c9b4388bb2b8b97796f3b8
Alexander Shaduri <ashaduri@gmail.com>
2011-11-19 18:08:58 +0000

    Merged FreeBSD patches (fix in gsmartcontrol-root, /dev/ada* detection.

--- gsmartcontrol/src/applib/storage_detector_other.cpp
+++ gsmartcontrol/src/applib/storage_detector_other.cpp
@@ -80,6 +80,7 @@ std::string detect_drives_other(std::vector<StorageDeviceRefPtr>& drives, Execut
 
 	whitelist.push_back("/^ad[0-9]+$/");  // adN without suffix - fbsd ide
 	whitelist.push_back("/^da[0-9]+$/");  // daN without suffix - fbsd scsi, usb
+	whitelist.push_back("/^ada[0-9]+$/");  // adaN without suffix - fbsd ata cam
 // 	whitelist.push_back("/	^sa[0-9]+$/");  // saN without suffix - fbsd scsi tape
 // 	whitelist.push_back("/^ast[0-9]+$/");  // astN without suffix - fbsd ide tape
 	whitelist.push_back("/^aacd[0-9]+$/");  // fbsd adaptec raid
