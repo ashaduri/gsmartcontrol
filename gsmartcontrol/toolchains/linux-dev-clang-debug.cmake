7075a4f4d50c06786d5fb3f0e43ac713f115356d
Alexander Shaduri <ashaduri@gmail.com>
2021-02-23 11:54:56 +0000

    More work on cmake support.

---
+++ gsmartcontrol/toolchains/linux-dev-clang-debug.cmake
@@ -0,0 +1,28 @@
+
+# Linux development version with clang, debug mode.
+# Use with:
+# cmake -DCMAKE_TOOLCHAIN_FILE=...
+
+set(CMAKE_SYSTEM_NAME Linux)
+
+# Specify the compiler
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+
+# Common options for gcc/clang
+include(${CMAKE_CURRENT_LIST_DIR}/common-gcc-clang.cmake)
+
+# Common options for development
+include(${CMAKE_CURRENT_LIST_DIR}/common-dev.cmake)
+
+# Clang-specific options
+add_compile_options(
+	-Wdocumentation
+	-Wheader-guard
+	-Wloop-analysis
+	-Wno-keyword-macro
+)
+
+
+# Enable clang-tidy by default, reading from <project_root>/.clang-tidy file
+set(CMAKE_CXX_CLANG_TIDY "clang-tidy")
