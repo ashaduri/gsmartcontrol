60d9bf19220d7718a9233d6c044ecae520c26a37
Alexander Shaduri <ashaduri@gmail.com>
2021-03-05 14:19:45 +0400

    Add separate toolchain with clang-tidy support.

--- toolchains/linux-dev-with-tidy.cmake
+++ toolchains/linux-dev-with-tidy.cmake
@@ -10,3 +10,7 @@ set(APP_COMPILER_ENABLE_WARNINGS ON)
 set(APP_BUILD_EXAMPLES ON)
 set(APP_BUILD_TESTS ON)
 
+
+# Enable clang-tidy by default, reading from <project_root>/.clang-tidy file
+set(CMAKE_CXX_CLANG_TIDY "clang-tidy")
+
