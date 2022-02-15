c59fa32efec111c591d75f55eb134c1a8dfc39b0
Alexander Shaduri <ashaduri@gmail.com>
2022-02-15 16:22:41 +0400

    Added a Boost LEAF library copy.

---
+++ dependencies/leaf/convenience/leaf_ns.h
@@ -0,0 +1,22 @@
+/******************************************************************************
+License: GNU General Public License v3.0 only
+Copyright:
+	(C) 2022 Alexander Shaduri <ashaduri@gmail.com>
+******************************************************************************/
+#ifndef APP_LEAF_NS_H
+#define APP_LEAF_NS_H
+
+#include "../standalone/leaf.hpp"
+
+
+/// Use leaf:: instead of boost::leaf for convenience
+namespace leaf {
+
+	using namespace boost::leaf;
+
+}
+
+
+#endif
+
+/// @}
