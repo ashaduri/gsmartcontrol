84867cc4747ac2b7b9b84884995e4bfbdf42a271
Alexander Shaduri <ashaduri@gmail.com>
2024-05-21 16:46:53 +0400

    Create and use cmake toolchains for msys2.

---
+++ toolchains/win64-mingw-msys2.cmake
@@ -0,0 +1,37 @@
+###############################################################################
+# License: BSD Zero Clause License file
+# Copyright:
+#   (C) 2024 Alexander Shaduri <ashaduri@gmail.com>
+###############################################################################
+
+# Windows 64-bit production version with mingw/msys2.
+# Use with:
+# cmake -DCMAKE_TOOLCHAIN_FILE=...
+
+# Target OS
+set(CMAKE_SYSTEM_NAME Windows)
+
+# Specify the compiler
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+#set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+# The target environment. This is a path which is valid for msys2.
+set(CMAKE_FIND_ROOT_PATH /mingw64)
+
+# Enable LTO for Release build
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
+
+# Increase optimizations of Release builds
+add_compile_options("$<$<CONFIG:RELEASE>:-g0 -O3 -s>")
+
+# Enable common CPU optimizations
+add_compile_options(-mtune=generic)
+
+
+# Developer options
+set(APP_COMPILER_ENABLE_WARNINGS ON)
+set(APP_BUILD_EXAMPLES ON)
+set(APP_BUILD_TESTS ON)
+
+
