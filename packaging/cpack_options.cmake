a7f513fd7c6914ed6f5a1073f485b56e19076805
Alexander Shaduri <ashaduri@gmail.com>
2024-05-28 14:07:20 +0400

    Fix CPack rules.

--- packaging/cpack_options.cmake
+++ packaging/cpack_options.cmake
@@ -189,7 +189,7 @@ if (WIN32)
 	# TODO unix2dos doc/*.txt
 
 	# Smartmontools
-	if (EXISTS "${CMAKE_BINARY_DIR}/smartmontools")  # CI
+	if (IS_DIRECTORY "${CMAKE_BINARY_DIR}/smartmontools")  # CI
 		# GitHub extract location. The files are extracted without relative paths in archive (7z e).
 		install(FILES
 			"${CMAKE_BINARY_DIR}/smartmontools/drivedb.h"
